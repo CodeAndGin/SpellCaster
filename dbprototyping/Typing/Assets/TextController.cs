69be4c16752c4aa727b3d2cb3a484ee022937ff3
Diarmaid Brennan <dbren002@gold.ac.uk>
2017-11-20 16:14:52 +0000

    Basic Typing Done

---
+++ dbprototyping/Typing/Assets/TextController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class TextController : MonoBehaviour {
+
+	public Text typing;
+
+	void Start () {
+		typing = GameObject.Find("Text").GetComponent<Text>();
+		typing.text = "";
+	}
+	
+	void Update () {
+		addToString();
+	}
+	
+	void addToString() {
+		foreach (char c in Input.inputString) {
+			if (Input.GetKeyDown(KeyCode.Return)) {
+				typing.text = "";
+				//castSpell();
+			} else {
+				typing.text = typing.text + c;
+			}
+		}
+	}
+}
