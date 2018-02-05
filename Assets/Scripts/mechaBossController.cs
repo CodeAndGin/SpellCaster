bb7bde37b970f8bdc78238dfb4d2f15992d65724
Aaron Blake Wiltshire <awilt002@gold.ac.uk>
2018-02-05 18:45:57 +0000

    boss can die now

--- Assets/Scripts/mechaBossController.cs
+++ Assets/Scripts/mechaBossController.cs
@@ -7,16 +7,31 @@ public class mechaBossController : MonoBehaviour {
     private Animator anim;
     public GameObject projectilePrefab;
     private GameObject movingProjectile;
-    public int health = 8;
+    public float health = 8;
+
+    private GameObject spawner;
+    private levelController levels;
 
     // Use this for initialization
     void Start () {
         anim = GetComponent<Animator>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+
+        spawner = GameObject.Find("EnemySpawner");
+        levels = GameObject.Find("LevelController").GetComponent<levelController>();
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        other.gameObject.SendMessage("enemyInteraction", gameObject);   //tells whatever trigger touches it to do the "enemyInteraction" function
+        if (other.gameObject.tag == "Projectile" || other.gameObject.tag == "Shield") other.gameObject.SendMessage("death"); //destroys Projectiles and shields
+    }
+
+    // Update is called once per frame
+    void Update () {
         StartCoroutine("attacking");
+
+        if (health < 1) death();
 	}
 
     IEnumerator attacking()
@@ -29,5 +44,36 @@ public class mechaBossController : MonoBehaviour {
         anim.SetBool("isWalking", false);
         movingProjectile = Instantiate(projectilePrefab, fist, Quaternion.identity) as GameObject;
         StopCoroutine("attacking");
+    }
+
+    void death()
+    {
+        /*aliveNoise.Stop();
+        if (playOnce == false)
+        {
+            deathAudio.volume = 0.5f;
+            deathAudio.Play();
+            playOnce = true;
+        }*/
+        Time.timeScale = 0;
+        StartCoroutine("deathSound");
+
+    }
+
+    void damage()
+    {
+        health -= 1f; //to be called by other scripts to damage the enemy
+    }
+
+    IEnumerator deathSound()
+    {
+        yield return new WaitForSecondsRealtime(1.5f);
+        spawner.gameObject.SendMessage("enemyIsDead");
+        
+        
+        levels.gameObject.SendMessage("nLevel");
+        Destroy(gameObject);
+
+        StopCoroutine("deathSound");
     }
 }
