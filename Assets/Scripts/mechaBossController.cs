b1aa761092dfc9724ae7a70793314d2c37ad6a67
Aaron Blake Wiltshire <awilt002@gold.ac.uk>
2018-02-05 15:59:53 +0000

    boss animations added to master

---
+++ Assets/Scripts/mechaBossController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mechaBossController : MonoBehaviour {
+
+    private Animator anim;
+    //bool isWalking;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        StartCoroutine("attacking");
+	}
+
+    IEnumerator attacking()
+    {
+        yield return new WaitForSeconds(3.0f);
+        anim.SetBool("isWalking", true);
+        //Debug.Log("true: " + isWalking);
+        yield return new WaitForSeconds(3.0f);
+        anim.SetBool("isWalking", false);
+        //Debug.Log("false: " + isWalking);
+        StopCoroutine("attacking");
+    }
+}
