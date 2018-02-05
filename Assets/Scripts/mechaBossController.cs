acbb24d68198503b3d54773b8cbf4e46a1d89c29
Aaron Blake Wiltshire <awilt002@gold.ac.uk>
2018-02-05 18:35:53 +0000

    spawns working

--- Assets/Scripts/mechaBossController.cs
+++ Assets/Scripts/mechaBossController.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class mechaBossController : MonoBehaviour {
 
     private Animator anim;
-    public int speed;
-    //bool isWalking;
+    public GameObject projectilePrefab;
+    private GameObject movingProjectile;
+    public int health = 8;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         anim = GetComponent<Animator>();
 	}
 	
@@ -20,11 +21,13 @@ public class mechaBossController : MonoBehaviour {
 
     IEnumerator attacking()
     {
+        Vector3 fist = new Vector3(gameObject.transform.position.x - 3f, -5.11f, 163.1f);
         yield return new WaitForSeconds(3.0f);
         anim.SetBool("isWalking", true);
-        transform.Translate(-Vector3.right * Time.deltaTime * speed);
+        transform.Translate(-Vector3.right * Time.deltaTime);
         yield return new WaitForSeconds(2f);
         anim.SetBool("isWalking", false);
+        movingProjectile = Instantiate(projectilePrefab, fist, Quaternion.identity) as GameObject;
         StopCoroutine("attacking");
     }
 }
---
+++ Assets/Scripts/projectileSpeedIncrease.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectileSpeedIncrease : MonoBehaviour {
+
+    
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.GetComponent<enemyProjectileMove>().speed *= 1.025f;
+    }
+}
