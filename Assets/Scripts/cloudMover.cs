2766a577e5e19b962fe2b89809e062331c384d7d
Aaron Wiltshire <awilt002@gold.ac.uk>
2018-02-28 02:21:22 +0000

    added animation to clouds and pause button with spell list

---
+++ Assets/Scripts/cloudMover.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cloudMover : MonoBehaviour {
+
+    //public GameObject birbPrefab;
+    //private GameObject movingBirb;
+    public float speed = 0.01f;
+    private Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(transform.right * speed);
+        if (transform.position.x > 25.0f) death();
+    }
+
+    public void death()
+    {
+        Destroy(gameObject);
+    }
+}
