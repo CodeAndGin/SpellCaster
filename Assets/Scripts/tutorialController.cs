f97f4d0b12a37f7062dfac2910e0994af44ce0b2
Aaron Blake Wiltshire <awilt002@gold.ac.uk>
2018-03-18 21:38:10 +0000

    Added tutorial text

---
+++ Assets/Scripts/tutorialController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutorialController : MonoBehaviour {
+    public GameObject text1;
+    public GameObject text2;
+    public GameObject text3;
+    public GameObject text4;
+    public GameObject text5;
+    public GameObject text6;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
