4a6a39822db92ca839a4263c0430e623abd26f55
Aaron Blake Wiltshire <awilt002@gold.ac.uk>
2018-03-18 21:03:56 +0000

    menu complete

---
+++ Assets/Scripts/menuController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuController : MonoBehaviour
+{
+    public GameObject anyText;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine("flashing");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            SceneManager.LoadScene("Level1");
+        }
+    }
+
+    IEnumerator flashing()
+    {
+        while (true) {
+            anyText.SetActive(false);
+            yield return new WaitForSeconds(0.5f);
+            anyText.SetActive(true);
+            yield return new WaitForSeconds(0.5f);
+            
+        }
+    }
+}
