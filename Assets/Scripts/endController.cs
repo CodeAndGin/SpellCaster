480410b6d27a0b85ea15c584939c1d2a2e374a55
Aaron Blake Wiltshire <awilt002@gold.ac.uk>
2018-03-19 02:15:49 +0000

    ending screen added and goblin sprite looks less horrific now

---
+++ Assets/Scripts/endController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class endController : MonoBehaviour {
+    public GameObject text1;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("Ending");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator Ending()
+    {
+        yield return new WaitForSeconds(2f);
+        text1.SetActive(true);
+        yield return new WaitForSeconds(5f);
+        SceneManager.LoadScene("menu");
+    }
+}
