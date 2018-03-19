5d6136d1aec708e19f276404e12d778827a3e251
Aaron Blake Wiltshire <awilt002@gold.ac.uk>
2018-03-19 00:34:26 +0000

    added new animated sprites and a full tutorial

--- Assets/Scripts/tutorialController.cs
+++ Assets/Scripts/tutorialController.cs
@@ -2,21 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class tutorialController : MonoBehaviour {
+public class tutorialController : MonoBehaviour
+{
+    private GameObject GameState;
     public GameObject text1;
     public GameObject text2;
     public GameObject text3;
     public GameObject text4;
     public GameObject text5;
     public GameObject text6;
-
+    public GameObject text7;
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start()
+    {
+        GameState = GameObject.Find("GameStateController");
+        StartCoroutine("TutorialText");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator TutorialText()
+    {
+        GameState.SendMessage("Pause");
+        text1.SetActive(true);
+        yield return new WaitForSecondsRealtime(4f);
+        text1.SetActive(false);
+        yield return new WaitForSecondsRealtime(1f);
+        text2.SetActive(true);
+        yield return new WaitForSecondsRealtime(4f);
+        text2.SetActive(false);
+        yield return new WaitForSecondsRealtime(0f);
+        GameState.SendMessage("Unpause");
+        yield return new WaitForSecondsRealtime(6f);
+        GameState.SendMessage("Pause");
+        yield return new WaitForSecondsRealtime(0f);
+        text3.SetActive(true);
+        yield return new WaitForSecondsRealtime(4f);
+        text3.SetActive(false);
+        yield return new WaitForSecondsRealtime(1f);
+        text4.SetActive(true);
+        yield return new WaitForSecondsRealtime(8f);
+        text4.SetActive(false);
+        yield return new WaitForSecondsRealtime(1f);
+        text5.SetActive(true);
+        yield return new WaitForSecondsRealtime(8f);
+        text5.SetActive(false);
+        yield return new WaitForSecondsRealtime(1f);
+        text6.SetActive(true);
+        yield return new WaitForSecondsRealtime(8f);
+        text6.SetActive(false);
+        yield return new WaitForSecondsRealtime(1f);
+        text7.SetActive(true);
+        yield return new WaitForSecondsRealtime(8f);
+        text7.SetActive(false);
+        yield return new WaitForSecondsRealtime(0f);
+        GameState.SendMessage("Unpause");
+    }
 }
