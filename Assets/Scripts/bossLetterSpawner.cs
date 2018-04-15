be1a20cb1d3d8c0f9fd804f76ed665262bbb789f
Diarmaid Brennan <dbren002@gold.ac.uk>
2018-04-15 23:50:57 +0100

    More or less done with some minor bugs

---
+++ Assets/Scripts/bossLetterSpawner.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bossLetterSpawner : MonoBehaviour {
+
+	private GameObject newLetter;
+	public GameObject a;
+	public GameObject b;
+	public GameObject cee;
+	public GameObject d;
+	public GameObject e;
+	public GameObject f;
+	public GameObject g;
+	public GameObject h;
+	public GameObject i;
+	public GameObject j;
+	public GameObject k;
+	public GameObject l;
+	public GameObject m;
+	public GameObject n;
+	public GameObject o;
+	public GameObject p;
+	public GameObject q;
+	public GameObject r;
+	public GameObject s;
+	public GameObject t;
+	public GameObject u;
+	public GameObject v;
+	public GameObject w;
+	public GameObject x;
+	public GameObject y;
+	public GameObject z;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void createNewLetter (char first) {
+		if (gameObject.name == "BossNameLetterSpawner (0)") {
+			spawnLetter(first);
+		} else {
+			string alphabet = "abcdefghijklmnopqrstuvwxyz";
+			char randomLetter = alphabet[Random.Range(0,26)];
+			spawnLetter(randomLetter);
+		}
+	}
+
+	void spawnLetter (char output) {
+		string convert = "";
+		convert = convert + output;
+		convert = convert.ToLower();
+		char c = convert[0];
+		/*
+			The lines above just convert any capsLocked keys to lower case so the letters still
+			spawn without issue. The same lines are in the TextController.cs script so that spells
+			can still be cast no matter the arrangement of uppercase and lowercase letters in the
+			typing variable
+		*/
+		if (c == 'a') {
+			newLetter = Instantiate(a, transform.position, Quaternion.identity) as GameObject;
+		} else if (c == 'b') {
+			newLetter = Instantiate(b, transform.position, Quaternion.identity) as GameObject;
+		} else if (c == 'c') {
+			newLetter = Instantiate(cee, transform.position, Quaternion.identity) as GameObject;
+		} else if (c == 'd') {
+			newLetter = Instantiate(d, transform.position, Quaternion.identity) as GameObject;
+		} else if (c == 'e') {
+			newLetter = Instantiate(e, transform.position, Quaternion.identity) as GameObject;
+		} else if (c == 'f') {
+			newLetter = Instantiate(f, transform.position, Quaternion.identity) as GameObject;
+		} else if (c == 'g') {
+			newLetter = Instantiate(g, transform.position, Quaternion.identity) as GameObject;
+		} else if (c == 'h') {
+			newLetter = Instantiate(h, transform.position, Quaternion.identity) as GameObject;
+		} else if (c == 'i') {
+			newLetter = Instantiate(i, transform.position, Quaternion.identity) as GameObject;
+		} else if (c == 'j') {
+			newLetter = Instantiate(j, transform.position, Quaternion.identity) as GameObject;
+		} else if (c == 'k') {
+			newLetter = Instantiate(k, transform.position, Quaternion.identity) as GameObject;
+		} else if (c == 'l') {
+			newLetter = Instantiate(l, transform.position, Quaternion.identity) as GameObject;
+		} else if (c == 'm') {
+			newLetter = Instantiate(m, transform.position, Quaternion.identity) as GameObject;
+		} else if (c == 'n') {
+			newLetter = Instantiate(n, transform.position, Quaternion.identity) as GameObject;
+		} else if (c == 'o') {
+			newLetter = Instantiate(o, transform.position, Quaternion.identity) as GameObject;
+		} else if (c == 'p') {
+			newLetter = Instantiate(p, transform.position, Quaternion.identity) as GameObject;
+		} else if (c == 'q') {
+			newLetter = Instantiate(q, transform.position, Quaternion.identity) as GameObject;
+		} else if (c == 'r') {
+			newLetter = Instantiate(r, transform.position, Quaternion.identity) as GameObject;
+		} else if (c == 's') {
+			newLetter = Instantiate(s, transform.position, Quaternion.identity) as GameObject;
+		} else if (c == 't') {
+			newLetter = Instantiate(t, transform.position, Quaternion.identity) as GameObject;
+		} else if (c == 'u') {
+			newLetter = Instantiate(u, transform.position, Quaternion.identity) as GameObject;
+		} else if (c == 'v') {
+			newLetter = Instantiate(v, transform.position, Quaternion.identity) as GameObject;
+		} else if (c == 'w') {
+			newLetter = Instantiate(w, transform.position, Quaternion.identity) as GameObject;
+		} else if (c == 'x') {
+			newLetter = Instantiate(x, transform.position, Quaternion.identity) as GameObject;
+		} else if (c == 'y') {
+			newLetter = Instantiate(y, transform.position, Quaternion.identity) as GameObject;
+		} else if (c == 'z') {
+			newLetter = Instantiate(z, transform.position, Quaternion.identity) as GameObject;
+		}
+
+		newLetter.transform.parent = transform;
+	}
+}
---
+++ Assets/Scripts/nameCreator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nameCreator : MonoBehaviour {
+
+	void MakeNewName (char first) {
+		GameObject[] Letters = GameObject.FindGameObjectsWithTag("enemyNameLetter");
+		foreach (GameObject letters in Letters) {
+			GameObject.Destroy(letters);
+		}
+		for (int i = 0; i <= 5; i++) {
+			transform.Find("BossNameLetterSpawner (" + i + ")").gameObject.SendMessage("createNewLetter", first);
+		}
+	}
+}
